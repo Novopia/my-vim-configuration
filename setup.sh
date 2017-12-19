f200ac56babaf6e90e1a802de9b8ac0495640820
Xiangyu Yao <xiangyu.yao24@gmail.com>
2017-12-18 22:11:17 -0500

    auto clone oh-my-zsh repo

--- setup.sh
+++ setup.sh
@@ -7,6 +7,10 @@ current_dir=$(pwd $0)
 
 files=$(find . -maxdepth 1 -not -name 'README.md' -not -name '.*' -not -name 'setup.sh')
 
+if [ ! [-a "~/.oh-my-zsh"]]; then
+  git clone "git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh"
+fi
+
 for file in $files; do
   prev_dotfile_path=$(echo $file | sed "s|^./|$HOME/.|")
   new_dotfile_path=$(echo $file | sed "s|^./|$current_dir/|")
@@ -19,3 +23,4 @@ for file in $files; do
   echo "SymLinking $new_dotfile_path ---> $prev_dotfile_path"
   ln -s "$new_dotfile_path" "$prev_dotfile_path"
 done
+
