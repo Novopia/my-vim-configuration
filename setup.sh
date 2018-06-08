7dd15ed836e875cadac4dc2a997250a31fbe7e6a
Xiangyu Yao <xiangyu.yao@mongodb.com>
2018-06-08 12:10:27 -0400

    Fix linux platform

--- setup.sh
+++ setup.sh
@@ -9,18 +9,19 @@ fi
 
 # $0 is the name of the bash script.
 CURDIR=$(pwd $0)
+COMMONDIR="cross-platform"
 
 if [ "$(uname)" == "Darwin" ]
 then
 	OSDIR="osx"
+	FILES=$(find $OSDIR -depth 1)
+	FILES+=" "$(find $COMMONDIR -depth 1)
 elif [ "$(uname)" == "Linux" ]
 then
 	OSDIR="linux"
+	FILES=$(find $OSDIR -maxdepth 1 -mindepth 1)
+	FILES+=" "$(find $COMMONDIR -maxdepth 1 -mindepth 1)
 fi
-
-COMMONDIR="cross-platform"
-FILES=$(find $OSDIR -depth 1)
-FILES+=" "$(find $COMMONDIR -depth 1)
 
 for CONF in $FILES
 do
@@ -30,10 +31,11 @@ do
   echo $SYSTEM_CONF
   echo $MY_CONF
 
+  # If the symlink already exists
   if [[ -h "$SYSTEM_CONF" ]]
   then
     echo "Removing existing $SYSTEM_CONF"
-    sudo rm -rf $SYSTEM_CONF
+    rm -rf $SYSTEM_CONF
   fi
 
   echo "SymLinking $MY_CONF ---> $SYSTEM_CONF"
