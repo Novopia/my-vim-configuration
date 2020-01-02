59ac1977bea7620b0acdaaddf5097fd7dcac17ea
Xiangyu Yao <yaoxiangyu@bytedance.com>
2020-01-02 14:14:23 +0800

    update

--- osx/zsh/bytedance.zsh
+++ osx/zsh/bytedance.zsh
@@ -1,1 +1,5 @@
-alias d="ssh yaoxiangyu@10.224.29.224"
+alias d="ssh yaoxiangyu@10.227.15.155"
+alias m="ssh tiger@10.19.61.160"
+alias zk="cd ~/projects/zookeeper-master"
+
+export ANT_OPTS=-Xmx512m
--- osx/zsh/mongo.zsh
+++ osx/zsh/mongo.zsh
@@ -2,7 +2,6 @@
 
 # Evergreen
 alias evergreen="~/bin/evergreen"
-alias m="cd ~/projects/mongo"
 alias glg="git log --stat --pretty='%C(bold)Commit:%C(reset) %C(green)%H%C(red)%d%n%C(bold)Author:%C(reset) %C(cyan)%an <%ae>%n%C(bold)Date:%C(reset)   %C(blue)%ai (%ar)%C(reset)%n%+B'"
 alias gb="git branch -vvvvv"
 alias gg="git grep"
